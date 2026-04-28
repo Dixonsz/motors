70dd4400e5426d66aba5ad5c3ef8b6608dc0976e
Dixon-SS03 <dixonsanchezsoza@gmail.com>
2026-04-28 01:37:01 -0600

    Normalizacion de tablas y sus iconos

--- Taller/apps/administracion/views/web/cita_web.py
+++ Taller/apps/administracion/views/web/cita_web.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from ...services.cita_service import CitaService
 from ...services.cliente_service import ClienteService
 from ...services.vehiculo_service import VehiculoService
@@ -106,4 +107,4 @@ def cita_eliminar(request, cita_id):
         except ValueError as exc:
             messages.error(request, str(exc))
 
-    return render(request, 'citas/citas_eliminar.html', {'cita': cita})
+    return render(request, 'confirmar_eliminacion.html', {'object': cita, 'cancel_url': reverse('citas_lista')})
